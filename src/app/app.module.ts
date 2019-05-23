10df90b98aab6ea7f80c03574c151a4712272d82
Dhaval Sheth <dhaval@dhavalsheth.com>
2019-05-23 15:34:26 -0400

    added bootstrap, jquery, components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
